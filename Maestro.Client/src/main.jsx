3d245f1721bbf839298e04b8d3b123dc2864be0a
berkay.acar <berkay.acar@duzey.com.tr>
2026-02-09 11:00:33 +0300

    Fix router basename for GitHub Pages

--- Maestro.Client/src/main.jsx
+++ Maestro.Client/src/main.jsx
@@ -8,7 +8,7 @@ import { AppProvider } from './context/AppContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </AppProvider>
